f0e58917ae8f05a8d1633eb6933b38825512626f
= <michal@debian>
2017-09-02 12:14:55 +0200

    remove index.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['book/showbook/all/(:num)'] = "catalog/product_lookup_by_id/$1";
